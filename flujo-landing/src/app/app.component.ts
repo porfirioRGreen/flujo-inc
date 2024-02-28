7fad800d284acfe0b9032b36256d925f3abf206d
Gisella Buffa <119527178+GisellaBuffa@users.noreply.github.com>
2024-02-28 17:29:16 -0300

    1-landing-page

--- flujo-landing/src/app/app.component.ts
+++ flujo-landing/src/app/app.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ContactFormGroup } from './models/contact-form-group';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-saveRegister() {
-throw new Error('Method not implemented.');
-}
+export class AppComponent implements OnInit {
   title = 'flujo-landing';
-  formGroup: any;
+  public formGroup: FormGroup<ContactFormGroup> = new FormGroup({
+    name: new FormControl(),
+    zipCode: new FormControl(),
+    property: new FormControl(),
+    email: new FormControl(),
+    phone: new FormControl(),
+    lastName: new FormControl(),
+  })
   protected saving = false
+  protected showGrid: boolean = true;
+  
+  ngOnInit(): void {
+    this.checkIfTheButtonIconsAreShown()
+  }
+
+  protected saveRegister = () => {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+  @HostListener('window:resize', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+  private checkIfTheButtonIconsAreShown(event?: any) {
+    this.showGrid = window.screen.width >= 1201
+  }
 }
--- flujo-landing/src/app/app.module.ts
+++ flujo-landing/src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { ImagesGridComponent } from './images-grid/images-grid.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule,
     AppRoutingModule,
     NgbModule,
