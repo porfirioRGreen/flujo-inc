ee5cd6e883e8dcd2b26b0b78c6fefdf55414369e
Gisella Buffa <119527178+GisellaBuffa@users.noreply.github.com>
2024-02-29 15:23:39 -0300

    Updated Registration Form Validations & Home Banner

--- flujo-landing/src/app/models/contact-form-group.ts
+++ flujo-landing/src/app/models/contact-form-group.ts
@@ -3,7 +3,7 @@ import { FormControl } from "@angular/forms"
 export interface ContactFormGroup {
   name: FormControl<null | string>
   zipCode: FormControl<null | number>
-  property: FormControl<null | string>
+  property: FormControl<null | number>
   email: FormControl<null | string>
   lastName: FormControl<null | string>
   phone: FormControl<null | string>
